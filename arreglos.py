e04d916b8abbaf66efe14c2777b89dde47e1e281
Diego Arturo Martinez Salazar <art_die@hotmail.com>
2020-04-24 23:52:56 -0500

    Add files via upload

---
+++ arreglos.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+#
+# array = np.random.randint(0,100,10)
+# number_max = array.max()
+# position = array.argmax()
+# min = array.min()
+# mediana = array.mean()
+#
+# print(array)
+# print(number_max)
+# print(position)
+# print(min)
+# print(mediana)
+#print(cambiar_forma)
+# print(array)
+# print(array[0,0])
+# print(array2)
+#print(array3)
+
+array = np.linspace(0,10,10)
+print(array)
